5b495d4574ed6150b0815bcf52b1714939854387
Yoga <jc051533@gmail.com>
2022-05-16 10:30:02 +0800

    Edit Notes

--- afternoonteaOrder/ViewController.swift
+++ afternoonteaOrder/ViewController.swift
@@ -22,24 +22,20 @@ class ViewController: UIViewController {
     
     var eggcake = "經典原味"
     var milktea = ""
-   // var sugar = "無糖"
-   // var ice = "去冰"
+    var sugar = "無糖"
+    var ice = "去冰"
     var order = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        resultLabel.text="請點單！"
     }
 
-    
-    @IBAction func orderName(_ sender: UITextField){
-        
-    }
-    
+    //選擇雞蛋糕口味
     @IBAction func changeFlavor(_ sender: UISegmentedControl) {
         if flavorSegment.selectedSegmentIndex==0{
             eggcake="經典原味"
-            
         }else if flavorSegment.selectedSegmentIndex==1{
             eggcake="奶香可可"
         }else{
@@ -48,10 +44,9 @@ class ViewController: UIViewController {
         }
     }
     
-    
+    //加購奶茶的switch
     @IBAction func yesOrnoSwitch(_ sender: UISwitch) {
         if milkteaSwitch.isOn{
-            
             sugarSlider.isHidden=false
             iceSlider.isHidden=false
             milktea="加購奶茶"
@@ -61,8 +56,9 @@ class ViewController: UIViewController {
             milktea=""
         }
     }
-    
+    //糖度調整
     @IBAction func changeSugar(_ sender: UISlider) {
+        //讓slider的數值皆為整數
         sugarSlider.value.round()
         if sugarSlider.value==1{
             sugarLabel.text="無糖"
@@ -79,8 +75,9 @@ class ViewController: UIViewController {
         }
         
     }
-    
+    //冰度調整
     @IBAction func changeIce(_ sender: UISlider) {
+        //讓slider的數值皆為整數
         iceSlider.value.round()
         if iceSlider.value==1{
             iceLabel.text="去冰"
@@ -94,9 +91,10 @@ class ViewController: UIViewController {
         
     }
     
-    
+    //送出訂單
     @IBAction func orderMeal(_ sender: Any) {
         let name = nameTextField.text!
+        //當name為空字串
         if name == ""{
             resultLabel.text="請輸入完整訂購資訊"
         }else if milkteaSwitch.isOn==false{
@@ -106,9 +104,12 @@ class ViewController: UIViewController {
             resultLabel.text=name+"今晚想來點：\n\n"+eggcake+"雞蛋糕\n\n"+milktea+sugarLabel.text!+iceLabel.text!
             
         }
+        //點選button收鍵盤
+        view.endEditing(true)
             
     }
     
+    //清除所有資料
     @IBAction func clearAll(_ sender: Any) {
         nameTextField.text=""
         flavorSegment.selectedSegmentIndex=0
